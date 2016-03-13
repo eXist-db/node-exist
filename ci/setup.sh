efe4d252ab50148efe07052aa0f4de43522acf25
Juri Leino <github@line-o.de>
2016-03-13 13:31:12 +0100

    fix (CI): cannot use $HOME

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env sh
-EXIST_DB_VERSION=eXist-3.0.RC1
 
 set -e
+cd ci
 # check to see if exist folder is empty
-if [ ! -d "$HOME/ci/exist-$EXIST_DB_VERSION" ]; then
+if [ ! -d "exist-$EXIST_DB_VERSION" ]; then
   wget https://github.com/eXist-db/exist/archive/${EXIST_DB_VERSION}.tar.gz -O /tmp/${EXIST_DB_VERSION}.tar.gz
-  cd ${HOME}/ci
   tar -xvf /tmp/${EXIST_DB_VERSION}.tar.gz
   cd exist-${EXIST_DB_VERSION}
   ./build.sh
--- ci/teardown.sh
+++ ci/teardown.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
-EXIST_DB_VERSION=eXist-3.0.RC1
 
-${HOME}/ci/exist-${EXIST_DB_VERSION}/bin/shutdown.sh
+ci/exist-${EXIST_DB_VERSION}/bin/shutdown.sh
