a47652ff2e5ad294a4e3ec693abd6c89b982849a
Juri Leino <github@line-o.de>
2016-03-13 13:20:27 +0100

    fix (CI): add ci scripts - show startup output

---
+++ ci/setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+EXIST_DB_VERSION=eXist-3.0.RC1
+
+set -e
+# check to see if exist folder is empty
+if [ ! -d "$HOME/ci/exist-$EXIST_DB_VERSION" ]; then
+  wget https://github.com/eXist-db/exist/archive/${EXIST_DB_VERSION}.tar.gz -O /tmp/${EXIST_DB_VERSION}.tar.gz
+  tar -xvf /tmp/${EXIST_DB_VERSION}.tar.gz
+  cd ${HOME}/ci/exist-${EXIST_DB_VERSION}
+  ./build.sh
+else
+  echo "Using cached database instance."
+fi
---
+++ ci/teardown.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+EXIST_DB_VERSION=eXist-3.0.RC1
+
+${HOME}/ci/exist-${EXIST_DB_VERSION}/bin/shutdown.sh
