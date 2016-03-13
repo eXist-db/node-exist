c8ae1fa238ed6300870e42b954f3732db26ccc30
Juri Leino <github@line-o.de>
2016-03-13 13:24:19 +0100

    fix (CI): startup script working directory

--- ci/setup.sh
+++ ci/setup.sh
@@ -5,8 +5,9 @@ set -e
 # check to see if exist folder is empty
 if [ ! -d "$HOME/ci/exist-$EXIST_DB_VERSION" ]; then
   wget https://github.com/eXist-db/exist/archive/${EXIST_DB_VERSION}.tar.gz -O /tmp/${EXIST_DB_VERSION}.tar.gz
+  cd ${HOME}/ci
   tar -xvf /tmp/${EXIST_DB_VERSION}.tar.gz
-  cd ${HOME}/ci/exist-${EXIST_DB_VERSION}
+  cd exist-${EXIST_DB_VERSION}
   ./build.sh
 else
   echo "Using cached database instance."
