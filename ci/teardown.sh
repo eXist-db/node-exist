fe1eb28a558bca42e501bd4a2121276b41fc484c
Juri Leino <github@line-o.de>
2016-03-14 10:19:26 +0100

    fix (CI): teardown shellscript

--- ci/teardown.sh
+++ ci/teardown.sh
@@ -2,7 +2,7 @@
 FOLDER=${HOME}/exist/${EXIST_DB_VERSION}
 
 set -e
-if [ ${EXIST_DB_VERSION} eq 'HEAD' ]; then
+if [ "${EXIST_DB_VERSION}" -eq "HEAD" ]; then
   # exclude HEAD from cache
   rm -rf ${FOLDER}
 else
