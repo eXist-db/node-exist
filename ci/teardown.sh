8a35c2b1dc6e9d0efdd41d833513345cf8ddee21
Juri Leino <github@line-o.de>
2016-03-14 10:51:15 +0100

    chore (CI): output info on before cache

--- ci/teardown.sh
+++ ci/teardown.sh
@@ -3,9 +3,9 @@ FOLDER=${HOME}/exist/${EXIST_DB_VERSION}
 
 set -e
 if [ "${EXIST_DB_VERSION}" -eq "HEAD" ]; then
-  # exclude HEAD from cache
+  echo "exclude HEAD from cache"
   rm -rf ${FOLDER}
 else
-  # reset logfiles for current version
+  echo "reset logfiles for ${EXIST_DB_VERSION}"
   rm -rf ${FOLDER}/webapp/WEB-INF/logs/*.log
 fi
