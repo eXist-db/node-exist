bd8623698753a28b7986162a39e758a0c1895edd
Juri Leino <github@line-o.de>
2017-02-09 13:49:39 +0100

    CI: use env var in teardown-db.sh

--- ci/teardown-db.sh
+++ ci/teardown-db.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
-FOLDER=${HOME}/exist/${EXIST_DB_VERSION}
 
 set -e
 if [ "${EXIST_DB_VERSION}" -eq "HEAD" ]; then
   echo "exclude HEAD from cache"
-  rm -rf ${FOLDER}
+  rm -rf ${EXIST_DB_FOLDER}
 else
   echo "reset data and logfiles for ${EXIST_DB_VERSION}"
   cd ${EXIST_DB_FOLDER}
