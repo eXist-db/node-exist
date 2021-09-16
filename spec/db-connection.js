fa7a4a9686877979875b2f40164ee72330049761
Juri Leino <github@line-o.de>
2021-09-16 16:38:03 +0200

    feat(test): override db-connection with env vars
    As for the examples before the same set of environment variables can
    now be used to alter the connection settings
    with which the tests are run.
    
    [db-connection.js](spec/db-connection.js) handles setting defaults and
    processing the server URL.
    
    For more details have a look at the
    [examples readme](spec/examples/readme.md).

--- spec/db-connection.js
+++ spec/db-connection.js
@@ -1,7 +1,15 @@
-module.exports = {
-  basic_auth: {
-    user: 'admin',
-    pass: ''
-  },
-  rejectUnauthorized: false
+const user = process.env.EXISTDB_USER || 'admin'
+const pass = process.env.EXISTDB_PASS || ''
+const server = process.env.EXISTDB_SERVER || 'http://localhost:8080'
+const { port, hostname, protocol } = new URL(server)
+const secure = protocol === 'https:'
+
+const connectionOptions = {
+  rejectUnauthorized: secure && hostname !== 'localhost',
+  secure,
+  basic_auth: { user, pass },
+  host: hostname,
+  port
 }
+
+module.exports = connectionOptions
