34172eca9b52ad1c5473ed4f8b11a2070275a41d
Juri Leino <github@line-o.de>
2022-02-02 23:14:38 +0100

    feat(collections): existsAndCanOpen
    Implements a call to collectionExistsAndCanOpen function
    provided by the XML RPC API of existdb.
    
    It will
    - return `true` if the collection exists and the current user can open it
    - return `false` if the collection does not exist
    - **throws** an exception if the collection exists
      but the current user cannot access it

--- spec/tests/collections.js
+++ spec/tests/collections.js
@@ -1,6 +1,10 @@
 const test = require('tape')
 const { connect } = require('../../index')
 const connectionOptions = require('../connection')
+const asGuest = Object.assign({},
+  connectionOptions,
+  { basic_auth: { user: 'guest', pass: 'guest' } }
+)
 
 test('get collection info', function (t) {
   const db = connect(connectionOptions)
@@ -60,6 +64,71 @@ test('remove collection', function (t) {
     })
     .catch(function (e) {
       t.fail(e, 'remove failed')
+      t.end()
+    })
+})
+
+test('collection exists and guest cannot open it', function (t) {
+  const db = connect(asGuest)
+  db.collections.existsAndCanOpen('/db/system/security')
+    .then(function () {
+      t.fail()
+      t.end()
+    })
+    .catch(function (e) {
+      t.ok(e, '/db/system/security exists and user guest cannot access it')
+      t.end()
+    })
+})
+
+test('collection exists and guest can open it', function (t) {
+  const db = connect(asGuest)
+  db.collections.existsAndCanOpen('/db/apps/dashboard')
+    .then(function (success) {
+      t.true(success, '/db/apps/dashboard exists and user guest can access it')
+      t.end()
+    })
+    .catch(function (e) {
+      t.fail(e)
+      t.end()
+    })
+})
+
+test('collection does not exist (guest)', function (t) {
+  const db = connect(asGuest)
+  db.collections.existsAndCanOpen('/db/apps/asdf')
+    .then(function (success) {
+      t.false(success, '/db/apps/asdf does not exist')
+      t.end()
+    })
+    .catch(function (e) {
+      t.fail(e)
+      t.end()
+    })
+})
+
+test('collection exists and admin can open it', function (t) {
+  const db = connect(connectionOptions)
+  db.collections.existsAndCanOpen('/db/system/security')
+    .then(function (success) {
+      t.true(success)
+      t.end()
+    })
+    .catch(function (e) {
+      t.fail(e)
+      t.end()
+    })
+})
+
+test('collection does not exist (admin)', function (t) {
+  const db = connect(connectionOptions)
+  db.collections.existsAndCanOpen('/db/apps/asdf')
+    .then(function (success) {
+      t.false(success, '/db/apps/asdf does not exist')
+      t.end()
+    })
+    .catch(function (e) {
+      t.fail(e)
       t.end()
     })
 })
