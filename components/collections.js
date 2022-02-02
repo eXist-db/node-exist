3d37d23db4c46b3b2456ffc3fd1bfa91a6764dce
Juri Leino <github@line-o.de>
2022-02-02 23:14:38 +0100

    feat(collections): existsAndCanOpen
    Implements a call to collectionExistsAndCanOpen function
    provided by the XML RPC API of existdb.
    
    It will
    - return `true` if the collection exists and the current user can open it
    - return `false` if the collection does not exist
    - **throws** an exception if the collection exists
      but the current user cannot access it

--- components/collections.js
+++ components/collections.js
@@ -18,9 +18,17 @@ function read (client, name) {
   return client.promisedMethodCall('getCollectionDesc', [name])
 }
 
+// convenience function
+// throws an exception if and only if
+// the collection exists but cannot be written by the user
+function existsAndCanOpen (client, name) {
+  return client.promisedMethodCall('existsAndCanOpenCollection', [name])
+}
+
 module.exports = {
   create,
   remove,
   describe,
-  read
+  read,
+  existsAndCanOpen
 }
